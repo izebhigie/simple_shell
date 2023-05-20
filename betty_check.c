7cc09927502b33fe33f47a5087f8263bfc0d30c1
izebhigie <dizebhigie@gmail.com>
2023-05-20 20:37:59 +0100

    task 0

---
+++ betty_check.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	if (a && b)
+	{
+		temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x = 10;
+	int y = 20;
+
+	printf("Before swap: x = %d, y = %d\n", x, y);
+	swap_int(&x, &y);
+	printf("After swap: x = %d, y = %d\n", x, y);
+
+	return (0);
+}
