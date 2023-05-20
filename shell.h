687468906cf461174ffcb370ca26d681d3eb9994
izebhigie <dizebhigie@gmail.com>
2023-05-20 19:45:26 +0100

    Shell.h

---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcnt1.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
